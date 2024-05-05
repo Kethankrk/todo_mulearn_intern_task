ceb0f244d11b79f2d7d1258cb5c76c5ed2ef53a4
Kethankrk <kethankrk@gmail.com>
2024-05-05 18:41:20 +0530

    added logout view, todo delete and done view

--- mvt/views.py
+++ mvt/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, redirect
-from . import forms
+from . import forms, models
 from django.contrib.auth import authenticate, login, logout
+from django.utils import timezone
 
 def homeView(request):
     user = request.user
-    print(user)
     if not user.is_authenticated:
         return redirect("login")
-    todos = user.todos.all()
-    context = {"todos": todos}
+    form = forms.TodoForm()
+    if request.method == "POST":
+        form = forms.TodoForm(data=request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.user = user
+            todo.save()
+            return redirect("home")
+    todos = user.todos.filter(completed=False)
+    context = {"todos": todos, "form": form}
     return render(request, 'mvt/index.html', context=context)
 
 
@@ -44,3 +52,33 @@ def loginView(request):
     context = {'login_form': form}
 
     return render(request, "mvt/login.html", context=context)
+
+def logoutView(request):
+    logout(request)
+    return redirect('login')
+
+def deleteTodoView(request):
+    if request.method == "POST":
+        todo_id = request.POST.get("todo_id")
+        todo = models.Todo.objects.get(id=todo_id)
+        todo.delete()
+        return redirect("home")
+    return redirect("home")
+
+
+def markTodoDoneView(request):
+    if request.method == "POST":
+        todo_id = request.POST.get("todo_id")
+        todo = models.Todo.objects.get(id=todo_id)
+        todo.completed = True
+        todo.completed_at = timezone.now()
+        todo.save()
+        return redirect("home")
+    return redirect("home")
+
+def todoLogView(request):
+    user = request.user
+
+    todos = user.todos.filter(completed=True)
+    context = {'todos': todos}
+    return render(request, 'mvt/todolog.html', context=context)
