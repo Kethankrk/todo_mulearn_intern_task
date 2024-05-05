913e83afee8ebd26bc2952026734048332e38fe9
Kethankrk <kethankrk@gmail.com>
2024-05-05 15:03:04 +0530

    added resigter and login forms and views

--- mvt/views.py
+++ mvt/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def homeView(request):
+    user = request.user
+    print(user)
+    if not user.is_authenticated:
+        return redirect("login")
+    todos = user.todos.all()
+    context = {"todos": todos}
+    return render(request, 'mvt/index.html', context=context)
+
+
+
+def registerView(request):
+    form = forms.CreateUserForm()
+
+    if request.method == "POST":
+        form = forms.CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request=request, user=user)
+                return redirect("home")
+    context = {'register_form': form}
+    return render(request, 'mvt/register.html', context=context)
+
+
+def loginView(request):
+    form = forms.LoginForm()
+
+    if request.method == "POST":
+        form = forms.LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request=request, user=user)
+                return redirect("home")
+    
+    context = {'login_form': form}
+
+    return render(request, "mvt/login.html", context=context)
